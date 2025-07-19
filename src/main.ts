871f49f5791cf454f8507ec5d21c507980ae1280
kingo <sijingl2013@outlook.com>
2025-07-19 09:52:27 +0800

    import iohook

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,27 @@ import './demos/ipc'
 // If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
 // import './demos/node'
 
+// Listen for iohook events from main process
+window.iohook?.onKeydown((msg) => {
+  console.log('keydown from main process', msg);
+});
+
+window.iohook?.onMousewheel((msg) => {
+  console.log('mousewheel from main process', msg);
+});
+
+window.iohook?.onMousedrag(() => {
+  console.log('mousedrag from main process');
+});
+
+window.iohook?.onMousedown((msg) => {
+  console.log('mousedown from main process', msg);
+});
+
+window.iohook?.onMouseup((msg) => {
+  console.log('mouseup from main process', msg);
+});
+
 createApp(App)
   .mount('#app')
   .$nextTick(() => {
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -10,3 +10,18 @@ interface Window {
   // expose in the `electron/preload/index.ts`
   ipcRenderer: import('electron').IpcRenderer
 }
+
+declare global {
+  interface Window {
+    iohook: {
+      onKeydown: (callback: (msg: any) => void) => void;
+      onMousewheel: (callback: (msg: any) => void) => void;
+      onMousedrag: (callback: () => void) => void;
+      onMousedown: (callback: (msg: any) => void) => void;
+      onMouseup: (callback: (msg: any) => void) => void;
+      removeAllListeners: (channel: string) => void;
+    };
+  }
+}
+
+export {}
